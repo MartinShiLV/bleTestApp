31a304ad060e75d58bef7abbb3cc8d763e6fc596
XIAOKAOBO <cobbshi810@outlook.com>
2022-06-29 11:53:33 -0400

    first

---
+++ pcap.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('test3.csv',header=0)
+lap_times = np.array([])
+# print(df.iloc[1,1])
+for row in df.itertuples():
+    if row.Protocol == 'ATT' and df.iloc[row[0]-1,4] == 'HCI_EVT':
+        start_index = row[0]-4
+        stop_index = row[0]
+        if start_index>=0 :
+            time_elapsed = df.iloc[stop_index,1] - df.iloc[start_index,1]
+            lap_times=np.append(lap_times,time_elapsed)
+        
+# print(lap_times)
+x_axis = np.arange(len(lap_times))
+y_axis = lap_times
+plt.plot(x_axis,y_axis)
+plt.show()
+ 
+
+
+        
+
+
+
